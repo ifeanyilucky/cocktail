5b6cccc3bd88a776912dcc95801433e13500127e
ifeanyilucky <wfguru2017@gmail.com>
2021-02-24 18:31:21 +0100

    error fixes

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,4 +1,16 @@
-const Layout = ({ children }) => {
-  return <div>{children}</div>;
+import Head from "next/head";
+const Layout = ({
+  children,
+  title = "Search drinks and cocktails from around the world.",
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </div>
+  );
 };
 export default Layout;
